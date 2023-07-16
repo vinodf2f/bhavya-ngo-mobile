82d77209be36eae28a2e36b92301acd5345d7d1a
Vinod <vinodyevatikar@Vinods-MacBook-Air.local>
2023-07-16 19:12:36 +0530

    intial setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <WebView
+        source={{ uri: "https://bhavya-ngo.vercel.app/" }}
+        style={{ marginTop: 15 }}
+      />
+      <StatusBar style="auto" backgroundColor="#ffffff" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
